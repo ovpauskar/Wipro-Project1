54eaffe3e2ca117d01ea9f3fae26a951494cc284
Darshan HN <darshanhn1991@gmail.com>
2018-06-20 17:09:51 +0530

    dev by Darshan
    Authenticated using Facebook and Twitter

---
+++ src/components/twitterAuthentication.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+
+//Assets
+import twitter from './twitter.png'
+
+import config from '../config';
+
+class TwitterLogin extends Component{
+    constructor(props) {
+        super(props)
+    }
+    
+    componentDidMount(){
+        (function() {
+            var e = document.createElement("script");
+            e.type = "text/javascript";
+            e.async = true;
+            e.src = "https://api.twitter.com/oauth/authenticate?oauth_token=NPcudxy0yU5T3tBzho7iCotZ3cnetKwcTIRlX0iwRl0"
+            var t = document.getElementsByTagName("script")[0];
+            t.parentNode.insertBefore(e, t)
+        })();   
+    }
+    
+    //Trigger Login for Twitter
+    twitterLogin = () => {
+        window.IN.init({
+            api_key : config.twitter
+        });
+        setTimeout(function(){
+                this.getUserDetails()
+            }.bind(this),1000);   
+        console.log( "Loaded" )
+    }
+    
+    getUserDetails = () => {
+        window.IN.User.authorize( function(){ 
+            window.IN.API.Profile("me")
+                .fields(["id", "firstName", "lastName", "pictureUrl", "publicProfileUrl"])
+                .result(function(result) {
+                    console.log(result);
+                    alert("Successfull login from twitter : "+ result.values[0].firstName + " " + result.values[0].lastName);
+                })
+                .error(function(err) {
+                    console.log('Import error - Error occured while importing data')
+                });
+        });
+    }
+    render(){
+        return(
+            <img src={twitter} title="twitter login" alt="twitter" onClick={ () => this.twitterLogin() }/>
+        )
+    }
+}
+
+export default TwitterLogin;
